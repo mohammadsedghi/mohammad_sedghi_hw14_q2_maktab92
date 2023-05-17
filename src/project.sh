562cfa196c15ab05ea1e13481e140a9f7d85187f
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-05-17 23:34:10 +0330

    wrong item is corrected

--- src/project.sh
+++ src/project.sh
@@ -1,36 +1,4 @@
 #!/bin/bash
-PS3="choose:"
-#select item in "created 5 file","add hello world to all file","replace bash with world"; do
-
-#case $item in
-#"created 5 file")
-
-#for i in {1..5}; do
-#   touch $i.txt
-# done
-#break
-#   ;;
-# "add hello world to all file")
-
-# for i in {1..5}; do
-#    echo "hello world" >$i.txt
-# done
-#break
-#  ;;
-#"replace bash with world")
-
-#for i in {1..5}; do
-# sed 's/world/bash/g' $i.txt | cat >$i.txt
-# done
-#break
-# ;;
-#esac
-#done
-#for i in {1..5} ; do
-#touch $i.txt
-# echo "hello world">$i.txt
-#sed 's/world/bash/g' $i.txt |cat >$i.txt
-#done
 PS3="Select item please: "
 
 items=("created 5 file" "add hello world to all file" "replace bash with world")
@@ -46,7 +14,7 @@ while true; do
       ;;
     2)
       for i in {1..5}; do
-        echo "hello world" >$i.txt
+        echo "hello world" >>$i.txt
       done
       break
       ;;
